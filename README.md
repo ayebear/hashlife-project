3a3b0b059e203b46ab2977e86eae02ed0bb1837a
Eric Hebert <ayebear@gmail.com>
2017-02-13 12:30:21 -0500

    Setup gulp web server

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Hashlife Project
+
+## Setup instructions
+
+```bash
+npm i -g gulp-cli
+npm i
+```
+
+## Build instructions
+
+```bash
+gulp
+```
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,17 @@
 var gulp = require('gulp');
 var shell = require('gulp-shell');
 // var rollup = require('gulp-rollup');
+var webserver = require('gulp-webserver');
 
 gulp.task('default', shell.task(
 	['rollup -c']
 ));
+
+gulp.task('run', function() {
+	gulp.src('./build/')
+		.pipe(webserver({
+			livereload: false,
+			directoryListing: false,
+			open: true
+		}));
+});
